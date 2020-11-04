74c63a8c768d2da6f099563ba59faa926ba9899c
Gregory Vicent <gregory142000@gmail.com>
2020-11-03 20:20:47 -0400

    My error jeje

--- src/components/comps_Home/CoverPage.jsx
+++ src/components/comps_Home/CoverPage.jsx
@@ -4,16 +4,66 @@ import "../../assets/styles/styles_Home/CoverPage.css"
 
 class CoverPage extends React.Component{
 
-    render(){
+    constructor(props){
+
+        super(props)
+
+        this.state = {
+
+            $p1: <p id="message-header-1" className="Cover-Page-p">¿Quienes Sómos?</p>,
+            $p2: <p id="message-header-2" className="Cover-Page-p delete">Sómos más que automóviles de lujo,<br/>Sómos una pasión</p>
+
+        }
+
+    }
+
+    componentDidMount(){
+
+        new Promise((resolve) => {
+
+            this.timerClass = setTimeout(() => {
+
+                resolve()
+
+                this.setState({
+
+                    $p1: <p id="message-header-1" className="Cover-Page-p delete">¿Quienes Sómos?</p>
+         
+                })
+
+            }, 3000)
+
+        }).then(() => {
+
+            this.timerClass2 = setTimeout(() => {
+
+                this.setState({
+
+                    $p2: <p id="message-header-2" className="Cover-Page-p">Sómos más que automóviles de lujo,<br/>Sómos una pasión</p>
+            
+                })
+
+            }, 1000)
+
+        }).catch()
+
+    }
+
+    componentWillUnmount(){
+
+        clearTimeout(this.timerClass)
+        clearTimeout(this.timerClass2)
+
+    }
+
+    render(){  
 
         return(
 
             <div className="Cover-Page">
-                <p>Sómos más que automóviles de lujo,<br/>Sómos una pasión</p>
-                <h2>¿Quienes Sómos?</h2>
-                <div className="door"></div>
+                {this.state.$p1}
+                {this.state.$p2}
             </div>
-
 
         )
 
