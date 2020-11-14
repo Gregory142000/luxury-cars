0b4e2c146d5303bf60deaccb8646b161ed8370e9
Gregory Vicent <gregory142000@gmail.com>
2020-11-14 19:33:11 -0400

    Ajustes al responsive desing de la pagina 'Cantact'

--- src/components/comps_Contact/ContactContent.jsx
+++ src/components/comps_Contact/ContactContent.jsx
@@ -7,7 +7,7 @@ function ContactContent(props){
     return(
 
         <section className="Contact-Content">
-            <p className="Contact-Text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea aliquid obcaecati quod vel amet repudiandae qui inventore dignissimos necessitatibus assumenda? Voluptatibus fugiat odit cumque eos officia dolore aspernatur eaque tempore saepe, nulla perferendis fuga explicabo architecto quos tempora maiores, debitis praesentium quis ea laborum ipsa omnis. Recusandae, perferendis! Mollitia iusto, recusandae animi amet sequi nesciunt sed quod quo itaque cupiditate natus quae illum aspernatur voluptate quasi perferendis eos possimus consequatur velit nihil quam veritatis? Architecto possimus unde aut, ad magnam repudiandae adipisci fugit provident porro? Ea fugiat rem modi dolorum rerum, ipsa sunt eligendi totam, asperiores incidunt maiores molestias eveniet?</p>
+            <p className="Contact-Content-Text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea aliquid obcaecati quod vel amet repudiandae qui inventore dignissimos necessitatibus assumenda? Voluptatibus fugiat odit cumque eos officia dolore aspernatur eaque tempore saepe, nulla perferendis fuga explicabo architecto quos tempora maiores, debitis praesentium quis ea laborum ipsa omnis. Recusandae, perferendis! Mollitia iusto, recusandae animi amet sequi nesciunt sed quod quo itaque cupiditate natus quae illum aspernatur voluptate quasi perferendis eos possimus consequatur velit nihil quam veritatis? Architecto possimus unde aut, ad magnam repudiandae adipisci fugit provident porro? Ea fugiat rem modi dolorum rerum, ipsa sunt eligendi totam, asperiores incidunt maiores molestias eveniet?</p>
         </section>
 
     )
