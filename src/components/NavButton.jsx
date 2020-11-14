2a3049822638532bedfae92af84d7f25b5a33874
Gregory Vicent <gregory142000@gmail.com>
2020-11-13 23:27:56 -0400

    Se creo e implemento el boton para la navegación en pantallas de telefonos

---
+++ src/components/NavButton.jsx
@@ -0,0 +1,49 @@
+import React from "react"
+import { Link } from "react-router-dom"
+
+import "../assets/styles/NavButton.css"
+import "../assets/hamburgers/dist/hamburgers.css"
+
+class NavButton extends React.Component{
+
+    activeScreenNav(){
+
+        document.querySelector(".screenNav").classList.toggle("screenNav-active")
+        document.querySelector(".my-hamburger").classList.toggle("is-active")
+
+    }
+
+    activeLink(){
+
+        document.querySelector(".screenNav").classList.remove("screenNav-active")
+        document.querySelector(".my-hamburger").classList.remove("is-active")
+
+    }
+
+    render(){
+
+        return(
+
+            <React.Fragment>
+                <button onClick={this.activeScreenNav} class="hamburger hamburger--arrow my-hamburger" type="button">
+                    <span class="hamburger-box">
+                        <span class="hamburger-inner"></span>
+                    </span>
+                </button>
+                <div className="screenNav">
+                    <ul className="list-NavButton">
+                        <li><Link onClick={this.activeLink} className="itemNavButton" to="/">Home</Link></li>
+                        <li><Link onClick={this.activeLink} className="itemNavButton" to="/gallery">Gallery</Link></li>
+                        <li><Link onClick={this.activeLink} className="itemNavButton" to="/team">Team</Link></li>
+                        <li><Link onClick={this.activeLink} className="itemNavButton" to="/contact">Contact</Link></li>
+                    </ul>
+                </div>
+            </React.Fragment>
+
+        )
+
+    }
+
+}
+
+export default NavButton
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import CoverPage from "../components/comps_Home/CoverPage"
 import GallerySection from "../components/comps_Home/GallerySection"
 import TeamSection from "../components/comps_Home/TeamSection"
 import ContactSection from "../components/comps_Home/ContactSection"
+import NavButton from "../components/NavButton"
 
 import "../assets/styles/styles_Home/Home.css"
 
@@ -14,6 +15,7 @@ class Home extends React.Component{
         return(
 
             <Fragment>
+                <NavButton />
                 <CoverPage />
                 <GallerySection />
                 <TeamSection />
